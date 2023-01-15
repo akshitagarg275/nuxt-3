b2c13423934773b04786444e89e1414ba4780e73
akshitagarg275 <akshitagarg275@gmail.com>
2023-01-16 00:32:25 +0530

    feat: routes and layouts added

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quod tenetur ex provident, veritatis magni explicabo commodi nam dolorem? Tenetur?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quod tenetur ex provident, veritatis magni explicabo commodi nam dolorem? Tenetur?</p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quod tenetur ex provident, veritatis magni explicabo commodi nam dolorem? Tenetur?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quod tenetur ex provident, veritatis magni explicabo commodi nam dolorem? Tenetur?</p>
+
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout : 'products'
+    })
+</script>
+
+<style scoped>
+
+</style>
